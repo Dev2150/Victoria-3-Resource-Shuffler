a989285edf7a8f1291db6e2599f7968d9c977141
Dev2150 <ademost95@yahoo.com>
2024-04-22 06:47:25 +0800

    Reads & stores resources in resouceKit

--- Main.py
+++ Main.py
@@ -72,11 +72,45 @@ def configureAppData():
     
     return pathAppdataStateRegions, pathGameStateRegions
 
-def readResourcesConfig():
-    pass
+def getStateCount():
+    states = 0
+    for filename in os.listdir(pathGameStateRegions):
+        if filename[0:2] == "99":
+            continue
+        filePath = os.path.join(pathGameStateRegions, filename)
+        if os.path.isfile(filePath):
+            with open(filePath, "r") as f:
+                for line in f:
+                    findState = re.search("(STATE_.*) =.*", line)
+                    if findState:
+                        states += 1
+    logger.info(f"Found {states} states in state_regions")
+    return states
 
-def getResourcesFromStateRegions(pathGameStateRegions):
+def getResourceKit(stateCount):
+    resourceKit = []
+    fileName = "resources.txt"
+    if not os.path.exists(fileName):
+        logger.error("File resource.txt does not exist")
+        exit
+    with open(fileName, "r") as f:
+        for line in f:
+            findResource = re.search(r"([_\w]+)\s+([_\w]+)", line)
+            if findResource == None:
+                logger.error(f"Error parsing line {line} in resources.txt")
+            else:
+                a = findResource.groups()[0]
+                b = findResource.groups()[1]
+                logger.info(f"{a} {b}")
+                resourceKit.append({'name': a, 
+                                    'buildingGroup': b, 
+                                    'states': [0] * stateCount})
+    return resourceKit
+
+def getResourcesFromStateRegions(resourceKit, pathGameStateRegions):
     logger.info(f"Reading files from {pathGameStateRegions}")
+    resourcesFound = 0
+    stateCurrent = -1
     for filename in os.listdir(pathGameStateRegions):
         if filename[0:2] == "99":
             continue
@@ -84,12 +118,23 @@ def getResourcesFromStateRegions(pathGameStateRegions):
         if os.path.isfile(filePath):
             logger.info(f"Reading state_region {filename}")
 
-            stateCurrent = None
             with open(filePath, "r") as f:
                 for line in f:
                     findState = re.search("(STATE_.*) =.*", line)
                     if findState:
-                        stateCurrent = findState
+                        stateCurrent += 1
+                    
+                    for idR, resource in enumerate(resourceKit):
+                        findResource = re.search(resource['buildingGroup'] + r" = (\d+)", line)
+                        if findResource:
+                            value = int(findResource.groups()[0])
+                            resourceKit[idR]['states'][stateCurrent] = value
+                            resourcesFound += value
+                        pass
+                        #resource.buildingGroup
+    
+    logger.info(f"Found {resourcesFound} resources in state_regions")
+    return resourceKit
                     
                         
 
@@ -97,5 +142,6 @@ def getResourcesFromStateRegions(pathGameStateRegions):
 if __name__ == "__main__":
     logger = setupLogging()
     pathAppdataStateRegions, pathGameStateRegions = configureAppData()
-    readResourcesConfig()
-    getResourcesFromStateRegions(pathGameStateRegions)
+    countStates = getStateCount()
+    resourceKit = getResourceKit(countStates)
+    resourceKit = getResourcesFromStateRegions(resourceKit, pathGameStateRegions)
